279a62355aa0703eef32b7b8996f0819204482bc
Knut H Pedersen <knuthp@gmail.com>
2014-12-31 18:53:13 +0100

    Changing to use slfj.

--- src/main/java/com/knuthp/microservices/trainstations/rt/RuterGateway.java
+++ src/main/java/com/knuthp/microservices/trainstations/rt/RuterGateway.java
@@ -7,8 +7,8 @@ import java.net.URL;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,7 +18,7 @@ import com.knuthp.microservices.reisapi.model.Place;
 @Component
 public class RuterGateway {
 	private static final String REISAPI = "reisapi.ruter.no";
-	private static Log LOG = LogFactory.getLog(RuterGateway.class);
+	private static Logger LOG = LoggerFactory.getLogger(RuterGateway.class);
 
 	public String getDeparturesJson(Place place) {
 		String url = "http://" + REISAPI + "/stopvisit/getdepartures/"
