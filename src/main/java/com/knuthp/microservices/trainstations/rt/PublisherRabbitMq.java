b90eb7a38edf469e761ed135acc0a96729220d53
Knut H Pedersen <knuthp@gmail.com>
2015-01-02 10:38:48 +0100

    Publish JSON

--- src/main/java/com/knuthp/microservices/trainstations/rt/PublisherRabbitMq.java
+++ src/main/java/com/knuthp/microservices/trainstations/rt/PublisherRabbitMq.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.knuthp.microservices.trainstations.rt.domain.RtDepartures;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -16,6 +17,7 @@ import com.rabbitmq.client.ConnectionFactory;
 
 @Component
 public class PublisherRabbitMq implements Publisher {
+	private static final String DEFAULT_AMQP_TX = "amqp://UITrHgLT:yGYKAijJ5OLNrY5ob0gXgFXN5oViPLGz@slow-vervain-44.bigwig.lshift.net:10922/ix2hR1tr2hmP";
 	private static final Logger logger = LoggerFactory
 			.getLogger(PublisherRabbitMq.class);
 	private static final String EXCHANGE_NAME = "train.stations.rt";
@@ -28,12 +30,13 @@ public class PublisherRabbitMq implements Publisher {
 	public void publishRtDepartures(RtDepartures rtDepartures) {
 		try {
 			ConnectionFactory factory = new ConnectionFactory();
-			factory.setUri("amqp://UITrHgLT:yGYKAijJ5OLNrY5ob0gXgFXN5oViPLGz@slow-vervain-44.bigwig.lshift.net:10923/ix2hR1tr2hmP");
+			factory.setUri(DEFAULT_AMQP_TX);
 			Connection connection = factory.newConnection();
 			Channel channel = connection.createChannel();
 
 			channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
-			String message = rtDepartures.toString();
+			ObjectMapper mapper = new ObjectMapper();
+			String message = mapper.writeValueAsString(rtDepartures);
 
 			channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes());
 			logger.info("Published " + rtDepartures.getPlaceId());
